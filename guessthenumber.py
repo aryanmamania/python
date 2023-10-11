4e56828cfd1cef4c7d0458bc81d3ace91b873f52
mamania0217 <aryanmamania17@gmail.com>
2023-10-11 23:39:01 +0530

    guess the number

---
+++ guessthenumber.py
@@ -0,0 +1,20 @@
+import random
+
+def guess_the_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess the number (1-100): "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+            break
+
+
+guess_the_number()
